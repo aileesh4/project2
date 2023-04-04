107f5b7e7c10eeec4ef65f50975ead3666462ea0
aileesh4 <aileeshjavier@gmail.com>
2023-04-04 19:35:10 -0400

    added auto text effect

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+//auto-text effect 
+const textEl = document.getElementById('text')
+const text = 'shop for clothing'
+let idx = 1
+let speed = 300 
+
+writeText()
+
+function writeText() {
+    textEl.innerText = text.slice(0, idx)
+
+    idx++
+
+    if(idx > text.length) {
+        idx = 1
+    }
+
+    setTimeout(writeText, 300)
+}
+
+//navbar
 let navbar = document.querySelector(".navbar");
 
 document.querySelector('#menu-bar').onclick = () =>{
@@ -7,3 +28,7 @@ document.querySelector('#menu-bar').onclick = () =>{
 window.onscroll = () =>{
     navbar.classList.remove('active');
 }
+
+
+
+
