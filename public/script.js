9b03895faf9ee2b016a4f4da3b21ad865c417a60
aileesh4 <aileeshjavier@gmail.com>
2023-04-04 22:36:57 -0400

    added ripple button effect

--- public/script.js
+++ public/script.js
@@ -18,6 +18,31 @@ function writeText() {
     setTimeout(writeText, 300)
 }
 
+//ripple-button effect
+const buttons = document.querySelectorAll('.btn')
+
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        const x = e.pageX
+        const y = e.pageY
+
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop
+
+        const circle = document.createElement('span')
+        circle.classList.add('circle')
+        circle.style.top = yInside + 'px'
+        circle.style.left = xInside + 'px'
+
+        this.appendChild(circle)
+
+        setTimeout(() => circle.remove(), 500)
+    })
+})
+
 //navbar
 let navbar = document.querySelector(".navbar");
 
